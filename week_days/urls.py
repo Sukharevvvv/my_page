f0b7a670ce504d7fee577765d4766c1d9e105c2a
Vladimir Sukharev <wolodun@mail.ru>
2023-07-04 23:53:17 +0400

    Use DTL and dynamic generation of context with using dataclasses

--- week_days/urls.py
+++ week_days/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:day_of_week>/', views.get_info_about_day_of_week_as_number),
-    path('<str:day_of_week>/', views.get_info_about_day_of_week),
+    path('day_of_week>/', views.get_info_about_day_of_week),
 ]
 
