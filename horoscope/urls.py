104d568dcb12ab65baa8d552790cb2ba5d2976d0
Vladimir Sukharev <wolodun@mail.ru>
2023-07-07 23:58:07 +0400

    Start use base template

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('type/<changed_element>/', views.element, name='element'),
     path('type/', views.element_types, name='element_type'),
-    path('', views.index),
+    path('', views.index, name='horoscope-index'),
     path('<int:sign_zodiac>/', views.get_info_about_zodiac_sign_by_number),
     path('<str:sign_zodiac>/', views.get_info_about_zodiac_sign, name='horoscope_name'),
 ]
