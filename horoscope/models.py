0003ab4c9ca31dae76987fe8fbb5de2d98659e93
Vladimir Sukharev <wolodun@mail.ru>
2023-07-30 00:22:12 +0400

    Add some JS to Horoscope app

--- horoscope/models.py
+++ horoscope/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+class Client(models.Model):
+    name = models.CharField(max_length=40)
+    sign = models.CharField()
+    def __str__(self):
+        return f'Имя- {self.name}, знак- {self.sign}'
 # Create your models here.
